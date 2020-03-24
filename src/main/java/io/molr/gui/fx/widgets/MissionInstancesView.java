b416a9b58d79c217c0a54c1ea16046123084b124
Stefan Krepp <s.krepp@.gsi.de>
2020-03-24 16:31:47 +0100

    add mission dispose support

--- src/main/java/io/molr/gui/fx/widgets/MissionInstancesView.java
+++ src/main/java/io/molr/gui/fx/widgets/MissionInstancesView.java
@@ -3,24 +3,33 @@ package io.molr.gui.fx.widgets;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import freetimelabs.io.reactorfx.schedulers.FxSchedulers;
 import io.molr.commons.domain.AgencyState;
+import io.molr.commons.domain.MissionCommand;
 import io.molr.commons.domain.MissionInstance;
+import io.molr.commons.domain.MissionState;
 import io.molr.gui.fx.commands.ViewMissionInstance;
 import io.molr.gui.fx.perspectives.MissionsPerspective;
 import io.molr.gui.fx.util.CellFactories;
 import io.molr.gui.fx.util.FormattedButton;
 import io.molr.mole.core.api.Mole;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+
 import org.minifx.workbench.annotations.Icon;
 import org.minifx.workbench.annotations.Name;
 import org.minifx.workbench.annotations.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import static freetimelabs.io.reactorfx.schedulers.FxSchedulers.fxThread;
 
 import javax.annotation.PostConstruct;
 
@@ -32,6 +41,8 @@ import static org.minifx.workbench.domain.PerspectivePos.LEFT;
 @Icon(value= FontAwesomeIcon.SITEMAP, color="blue" )
 @Order(2)
 public class MissionInstancesView extends BorderPane {
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(MissionInstancesView.class);
 
     @Autowired
     private Mole mole;
@@ -42,7 +53,11 @@ public class MissionInstancesView extends BorderPane {
     private ListView<MissionInstance> listView;
 
     private final ObservableList<MissionInstance> activeMissions = FXCollections.observableArrayList();
-
+    
+    private FormattedButton disposeButton;
+    
+    private Disposable selectedMissionInstanceStatesSubscription;
+    
     @PostConstruct
     public void init() {
         mole.states().map(AgencyState::activeMissions).publishOn(FxSchedulers.fxThread()).subscribe(ms -> activeMissions.setAll(ms));
@@ -60,13 +75,41 @@ public class MissionInstancesView extends BorderPane {
                     .subscribe(publisher::publishEvent);
         });
         buttons.getChildren().add(connectButton.getButton());
+        
+        disposeButton = new FormattedButton("Dispose", "Dispose", "Green");
+        disposeButton.getButton().setDisable(true);
+        disposeButton.getButton().setOnAction(event -> {
+            MissionInstance selectedInstance = listView.getSelectionModel().getSelectedItem();
+            mole.instruct(selectedInstance.handle(), MissionCommand.DISPOSE);
+        });
+        buttons.getChildren().add(disposeButton.getButton());
+        
+        listView.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<>() {
+            @Override
+            public void onChanged(Change<? extends MissionInstance> c) {
+                disposeButton.getButton().setDisable(true);
+                if(c.getList().size()==1) {
+                    MissionInstance selectedInstance = c.getList().get(0);
+                    onSelectedMissionInstanceChange(selectedInstance);
+                }
+            }
+        });
+        
         setBottom(buttons);
-
-
+    }
+    
+    private void onSelectedMissionInstanceChange(MissionInstance selectedInstance) {
+        if(selectedMissionInstanceStatesSubscription!=null) {
+            selectedMissionInstanceStatesSubscription.dispose();
+        }
+        Flux<MissionState> selectedMissionInstanceStatesFlux = mole.statesFor(selectedInstance.handle()).publishOn(fxThread());
+        selectedMissionInstanceStatesSubscription = selectedMissionInstanceStatesFlux.subscribe(this::onSelectedMissionStateUpdate);
     }
 
-    private void reloadListView(){
-        init();
+    private void onSelectedMissionStateUpdate(MissionState missionState) {
+        boolean disableDisposeButton = !missionState.allowedMissionCommands().contains(MissionCommand.DISPOSE);
+        disposeButton.getButton().setDisable(disableDisposeButton);
+        LOGGER.info((disableDisposeButton? "disable":"enable")+" dispose button on mission state update");
     }
 
     private ListView<MissionInstance> newListView() {
